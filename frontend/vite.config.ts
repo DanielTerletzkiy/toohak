e7aa10983135d95621e04e939b5681b9855077cf
daniel.terletzkiy <daniel.terletzkiy@check24.de>
2023-07-03 10:32:24 +0200

    added proxy from frontend to backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    server: {
+        proxy: {
+            "/api": "localhost:3000",
+            "/socket.io": {
+                target: "ws://localhost:3080",
+                ws: true,
+            },
+        },
+    }
 })
