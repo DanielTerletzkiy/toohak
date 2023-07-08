86205f8b62f0f2fc7d2321016623ba1fd4e48a9c
danielterletzkiy <danielterletzkiy@gmail.com>
2023-07-09 00:25:51 +0200

    randomize button grid placement

--- frontend/src/components/quiz/choices/button/ButtonGrid.vue
+++ frontend/src/components/quiz/choices/button/ButtonGrid.vue
@@ -1,15 +1,28 @@
 <script setup lang="ts">
-import DGrid from "vuelize/src/components/flex/DGrid.vue"
+import DGrid from "vuelize/src/components/flex/DGrid.vue";
 import Button from "./Button.vue";
-import {Answer} from "../../../../../../backend/shared/enums/Answer";
+import { Answer } from "../../../../../../backend/shared/enums/Answer";
 
-const props = defineProps({size: {type: String, default: '200px'}})
+const props = defineProps({ size: { type: String, default: "200px" } });
+console.log(Object.keys(Answer));
+
+function shuffleButtons(keys: (keyof Answer)[]): Answer[] {
+  for (let i = keys.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [keys[i], keys[j]] = [keys[j], keys[i]];
+  }
+  return keys.map((key) => Answer[key]);
+}
 </script>
 
 <template>
   <d-card class="wrapper" rounded="xl" background-color="transparent">
     <DGrid columns="2" gap="4">
-      <Button v-for="type in Answer" :type="type" :size="size"/>
+      <Button
+        v-for="type in shuffleButtons(Object.keys(Answer) as (keyof Answer)[])"
+        :type="type"
+        :size="size"
+      />
     </DGrid>
   </d-card>
 </template>
@@ -21,6 +34,5 @@ const props = defineProps({size: {type: String, default: '200px'}})
   width: $length;
   height: $length;
   overflow: hidden;
-
 }
 </style>
